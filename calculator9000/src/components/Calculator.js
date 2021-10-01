ff893cf9cc9e18b715af4607dd77ace569c11257
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-10-01 17:02:38 +0200

    calculette entre composant ok

--- calculator9000/src/components/Calculator.js
+++ calculator9000/src/components/Calculator.js
@@ -4,104 +4,53 @@ import '../style/AmazingNumberButton.css';
 import TheTitle from './TheTitle'
 import React, { useState } from 'react';
 
-// function ItSOverNineThousand(props){
-  //       return (
-  //           <div className='ItSOverNineThousand'>
-  //               <p>{props.value}</p>
-  //           </div>
-  //       )
-  // } 
 
 
+function BeautifullScreen({chiffre, setChiffre}){
 
-
-
-
-
-function TouchNumber () {
-
-  var chiffre = [];
-  for (var i = 0; i < 10; i++) {
-    chiffre.push(<button className='number_boutton' key={i}>{i}</button>);
-  }
-  return chiffre;
-
-}
-
-
-
-
-
-
-
-function BeautifullScreen({chiffre}){
-
-      return (
-          <div className='containeur_containeurs_boutton'>
+      return  <div className='containeur_containeurs_boutton'>
               <div className='containeur_boutton'>
                   <div id="ecran" className='mon_ecran'>{chiffre}</div>
               </div>
           </div>
-      )
+      
 } 
 
 
 
-function ChiffreAffiche(){
-
-  const [chiffre, setChiffre] = useState('0')
-
-  const affichage = function (a) {
-    setChiffre(c => c + a)
-  }
-
-  return [chiffre, affichage]
-}
-
-
-function TouchOperation () {
-
-  const [chiffre, affichage] = ChiffreAffiche()
-
-  const handleClick = function(a){
-    affichage(a)
-  }
+function TouchOperation ({chiffre, setChiffre}) {
 
   var operateurs = ['/', '*', '-', '+'];
-  const BouttonOperateur = operateurs.map(a => <button onClick={()=>{handleClick(a)}} key={a.toString()} className='number_boutton'>{a}</button>)
+  const BouttonOperateur = operateurs.map((a,index) => <button onClick={()=>setChiffre(chiffre + a)} key={`${a}-${index}`} className='number_boutton'>{a}</button>)
 
   return <div>
-    <BeautifullScreen chiffre={chiffre}></BeautifullScreen>
+    
     {BouttonOperateur}
 
   </div>
 
-
 }
 
+function TouchNumber ({chiffre, setChiffre}) {
 
 
 
-
-
-
-
-
-
-function TouchEgal () {
-
-  var egal = ['=', 'DEL'];
-  const bouttonOperateur = egal.map(c => <button key={c.toString()} className='number_boutton'>{c}</button>)
-  return bouttonOperateur;
+  var bouttonChiffre = [];
+  for (var i = 0; i < 10; i++) {
+    bouttonChiffre.push(<button onClick={(e)=>setChiffre(chiffre + e.target.innerHTML)} className='number_boutton' key={i}>{i}</button>);
+  }
+  return bouttonChiffre;
 
 }
 
 function Calculator () {
 
+  const [chiffre, setChiffre] = useState('0')
+
   return <div>
-    <TouchNumber/>
-    <TouchOperation/>
-    <TouchEgal/>
+    <BeautifullScreen chiffre={chiffre} setChiffre={setChiffre}/>
+    <TouchOperation chiffre={chiffre} setChiffre={setChiffre}/>
+    <TouchNumber chiffre={chiffre} setChiffre={setChiffre}/>
   </div>
 
 
